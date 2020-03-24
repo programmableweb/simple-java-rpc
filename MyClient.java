aba037d2ea3cbde39c15b3a7120f86872b76807d
Bob Reselman <reselbob@gmail.com>
2020-03-24 14:10:07 -0700

    Commented client code

--- MyClient.java
+++ MyClient.java
@@ -12,14 +12,21 @@ public class MyClient {
 
 	public static void main(String[] args) 
 		throws MalformedURLException, RemoteException, NotBoundException {
-		
+		// Query to Remote Object Regsistry to get a reference to the server
+		// that has the remote method
 		look_up = (RMIInterface) Naming.lookup("//localhost:5000/MyServer");
 
+		// Ask the user input into the console a message to echo
 		System.out.print("Enter a message: ");
+		
+		// Take the input and assign it to a string variable
 		String input = System.console().readLine();
 			
+		// Call the remote method, echo, using the reference to the
+		// server obtained from the Remote Object Registry
 		String response = look_up.echo(input);
 
+        // Output the response for the remote method back to the user
 		System.out.print(response + "\n");
 	}
 
